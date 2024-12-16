5f224ebd3d68cacf52fdace45509d20e0e157390
Polina <polinatrizna8@gmail.com>
2024-12-16 19:50:31 +0300

    Add first task of homework lesson 6 in CreditCard.java

---
+++ src/lesson_6_oop_basic_homework/CreditCard.java
@@ -0,0 +1,62 @@
+package lesson_6_oop_basic_homework;
+
+
+public class CreditCard {
+
+    // class fields or variables
+    public String accountNumber;
+    public double amountAccount;
+
+    // constructor to initialize objects
+    public CreditCard(String accountNumber, double amountAccount) {
+        this.accountNumber = accountNumber;
+        this.amountAccount = amountAccount;
+    }
+
+    // method, adding the amount
+    public void AddMoney (double someAmount) {
+        System.out.println("--------------Adding money--------------\n" + "Card: " + accountNumber);
+        System.out.println("Initial amount is " + amountAccount);
+        this.amountAccount += someAmount;
+        System.out.println("Adding "+ someAmount + "...");
+        System.out.println("Result is "+ amountAccount);
+    }
+
+    // method, withdrawing the amount
+    public void WithdrawMoney (double someAmount) {
+        System.out.println("----------Withdrawing money-----------\n" + "Card: " + accountNumber);
+        System.out.println("Initial amount is " + amountAccount);
+        this.amountAccount -= someAmount;
+        System.out.println("Withdraw " + someAmount + "...");
+        System.out.println("Result is " + amountAccount);
+
+    }
+
+    // method, getting card's information
+    public void SetInformationAboutCard (){
+        System.out.println("---------------------------------------------");
+        System.out.println("The number account card: " + accountNumber + "\nThe amount of money: " + amountAccount);
+    }
+
+    public static void main(String[] args) {
+
+        // Creating three objects
+        CreditCard firstNumberCard = new CreditCard("0000-2564-5265-2516",250.62);
+        CreditCard secondNumberCard = new CreditCard("89546-5895-0125-2356", 111.17);
+        CreditCard thirdNumberCard = new CreditCard("2564-0000-25648-0215", 1458.02);
+
+        // operation with money
+        firstNumberCard.AddMoney(25.52);
+        secondNumberCard.AddMoney(13.4);
+        thirdNumberCard.WithdrawMoney(5.82);
+
+        // get current card's information
+        firstNumberCard.SetInformationAboutCard();
+        secondNumberCard.SetInformationAboutCard();
+        thirdNumberCard.SetInformationAboutCard();
+
+
+    }
+
+
+}
