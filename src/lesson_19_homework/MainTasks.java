ff57db72dc1663cf48f92e8493e6fa890734c80f
Polina <polinatrizna8@gmail.com>
2025-03-12 22:12:24 +0300

    set homework task * in 20 package

---
+++ src/lesson_19_homework/MainTasks.java
@@ -0,0 +1,21 @@
+package lesson_19_homework;
+
+public class MainTasks {
+
+    //Создайте два отдельных ArrayList, где будет сохранены имена городов с буквой V и имена городов с буквой M
+    // Нужно через Стрим, задать фильтр и значение таким образом, чтобы в новом ArrayList он сохранил только
+    // определенные значения
+    public static void main(String[] args) {
+        setTheTask();
+
+    }
+
+    public static void setTheTask(){
+
+
+
+
+    }
+
+
+}
